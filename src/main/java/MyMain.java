1c4b24dc214b33200cebe614e0e91191b782829a
mawilbrink <martawilbrink@gmail.com>
2020-09-17 02:33:20 +0000

    wrote the binarytodecimal method

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,28 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        
+        int intBinary = Integer.parseInt(binary);
+        int decimal = 0;
+        int n = 0;
+    
+        while (intBinary != 0){
+            decimal += (intBinary%10)*(Math.pow(2,n));
+            intBinary /= 10;
+            n += 1;
+
+        }
+        
+        return decimal;
+        
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format:");
+
+        String input = scan.nextLine();
+        System.out.println("The decimal form of "+input+" is "+binaryToDecimal(input));
     }
 }
